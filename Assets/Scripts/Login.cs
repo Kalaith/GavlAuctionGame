08bf9aeb293bfbc021c41268f247fbe88aa8af06
Xiang Wan <37176328+cloudwxiang@users.noreply.github.com>
2018-04-20 00:07:38 +1000

    Add files via upload

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Login : MonoBehaviour
+{
+	public GameObject username;
+	public GameObject email;
+	public GameObject password;
+	public GameObject confPassword;
+	// Use this for initialization
+
+	public void LoginBtn(){
+
+		print("Sign Up Successful");
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown<InputField> (KeyCode.Tab)) {
+			if (username.GetComponent<InputField> ().isFocused) {
+				password.GetComponent<InputField> ().Select ();
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Return)){
+			if(Password != ""&& Password !=""){
+				LoginButton ();
+			}
+		}
+		Username = username.GetComponent<InputField> ().text;
+		Password = password.GetComponent<InputField> ().text;
+	}
+}
---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour {
+
+	public GameObject username;
+	public GameObject email;
+	public GameObject password;
+	public GameObject confPassword;
+	private string Username;
+	private string Email;
+	private string Password;
+	private string ConfPassword;
+	private string form;
+	private bool EmailValid = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void SignupButton(){
+		print("Sign Up Successful");
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown<InputField> (KeyCode.Tab)) {
+			if (username.GetComponent<InputField> ().isFocused) {
+				email.GetComponent<InputField> ().Select ();
+			}
+			if (email.GetComponent<InputField> ().isFocused) {
+				password.GetComponent<InputField> ().Select ();
+			}
+			if (password.GetComponent<InputField> ().isFocused) {
+				confPassword.GetComponent<InputField> ().Select ();
+			}
+		}
+
+		if(Input.GetKeyDown(KeyCode.Return)){
+			if(Password != ""&& Email !=""&& Password !=""&& ConfPassword !=""){
+				SignupButton ();
+			}
+		}
+
+		Username = username.GetComponent<InputField> ().text;
+		Email = email.GetComponent<InputField> ().text;
+		Password = password.GetComponent<InputField> ().text;
+		ConfPassword = confPassword.GetComponent<InputField> ().text;
+	}
+}
