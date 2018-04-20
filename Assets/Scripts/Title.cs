34414d45a20e34c8433917c1305b84d33a8a068c
Xiang Wan <37176328+cloudwxiang@users.noreply.github.com>
2018-04-20 20:31:13 +1000

    Add files via upload

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Title : MonoBehaviour {
+
+	public GameObject Button1;
+	// Use this for initialization
+	void Start () {
+		Button1 = transform.FindChild ("Back").gameObject;
+		UIEventListener.Get (Button1).onClick += TestButton;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void TestButton(GameObject go){
+		Debug.Log ("Preparing for you" + go.HowToPlay);
+	}
+}
